498033b2da6d4f0697fc52ec027e9a1c9e65993a
danjung9 <danielt923@gmail.com>
2025-11-21 17:25:09 -0500

    layed out TO DOs

--- pipelines/__init__.py
+++ pipelines/__init__.py
@@ -1,15 +1,1 @@
-"""High-level orchestration pipelines for requirements analysis."""
 
-from .compliance_pipeline import (
-    CompliancePipeline,
-    CompliancePipelineConfig,
-    ComplianceMatrixEntry,
-    JiraTicket,
-)
-
-__all__ = [
-    "CompliancePipeline",
-    "CompliancePipelineConfig",
-    "ComplianceMatrixEntry",
-    "JiraTicket",
-]
